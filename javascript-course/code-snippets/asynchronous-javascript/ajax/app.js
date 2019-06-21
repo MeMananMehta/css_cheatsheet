261ca439b173d6254d263c3e9815af9aa6db4231
Manan <mananmehta.work@gmail.com>
2019-06-21 16:30:16 +0530

    javascript

---
+++ javascript-course/code-snippets/asynchronous-javascript/ajax/app.js
@@ -0,0 +1,18 @@
+document.getElementById("button").addEventListener("click", loadData);
+
+function loadData() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "data.txt", true);
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      document.getElementById("output").textContent = this.responseText;
+    }
+  };
+
+  xhr.onerror = function() {
+    console.log("Request error...");
+  };
+
+  xhr.send();
+}
