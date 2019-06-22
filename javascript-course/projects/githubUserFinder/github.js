91788d7b95574852a6a5317f59ec2a2cb0790fd8
Manan <mananmehta.work@gmail.com>
2019-06-22 16:09:46 +0530

    project

---
+++ javascript-course/projects/githubUserFinder/github.js
@@ -0,0 +1,32 @@
+class GitHub {
+  constructor() {
+    this.client_id = "1ccaf63653c09da27208";
+    this.client_secret = "382ad9b440e6af6f61d6b47026cf7f93cc75b723";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
